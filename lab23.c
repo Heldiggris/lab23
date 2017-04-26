d7159e32b4c5a2d30e6f60238d51f862d0a17308
Heldiggris <potenkog@gmail.com>
2017-04-26 17:11:30 +0300

    Освобождение памяти и при аварийном выходе

--- lab23.c
+++ lab23.c
@@ -2,26 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tree.h"
+#include <signal.h>
+#include <stdbool.h>
+
+bool free_memory = false;
+
+
+
+void fct(int sign)
+{
+    printf("123\n");;
+}
+
+void free_memory(int sgn)
+{
+    free_memory = true;
+}
 
 int main(void)
 {
-    Node *tree = create_tree(1);
+    Node *tree = NULL;
+    signal(SIGINT, free_memory);
     while (1) {
+        if (free_memory) {
+            tree_destroy(tree);
+            return 0;
+        }
         char what_do[24];
         scanf("%s", what_do);
-        if (strcmp(what_do, "add") == 0) {
+        if (!strcmp(what_do, "add")) {
             int a, b;
             scanf("%d%d", &a, &b);
             node_add(tree, a, b);
-        } else if (strcmp(what_do, "draw") == 0) {
+        } else if (!strcmp(what_do, "root")) {
+            if (tree == NULL) {
+                int a;
+                scanf("%d", &a);
+                tree = create_tree(a);
+            }
+        } else if (!strcmp(what_do, "draw") || !strcmp(what_do, "print")) {
             tree_draw(tree, 0);
-        } else if (strcmp(what_do, "remove") == 0) {
+        } else if (!strcmp(what_do, "remove")) {
             int a;
             scanf("%d", &a);
             node_remove(tree, a);
-        } else if (strcmp(what_do, "exit") == 0 || strcmp(what_do, "close") == 0) {
+        } else if (!strcmp(what_do, "exit") || !strcmp(what_do, "close") || !strcmp(what_do, "quit")) {
             break;
-        } else if (strcmp(what_do, "result") == 0) {
+        } else if (!strcmp(what_do, "result")) {
             int matr[MAX_DEEP] = {0};
             int max = 0;
             int max_ind = 0;
@@ -36,9 +63,7 @@ int main(void)
             }
             printf("%d\n", max_ind + 1);
         } else {
-            tree_destroy(tree);
             printf("Error: Invalid query\n");
-            exit(1);
         }
     }
     tree_destroy(tree);
